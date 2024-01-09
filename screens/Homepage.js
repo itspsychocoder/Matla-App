27d16887096cd2c25fbedfa2da1b6111bb5f51fa
Hussnain Ahmad <hussnainahmad1606@gmail.com>
2024-01-09 21:36:35 +0500

    Design Profile Screen

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Font from 'expo-font';
+import { useEffect, useState } from 'react';
 export default function Homepage() {
+ 
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+  useEffect(() => {
+    const loadCustomFont = async () => {
+      try {
+        await Font.loadAsync({
+          CustomFont: require('../assets/fonts/cabin.ttf'),
+        });
+        console.log("Loaded")
+        setIsFontLoaded(true);
+      } catch (error) {
+        console.error('Error loading font:', error);
+        setIsFontLoaded(false);
+      }
+    };
+
+    loadCustomFont();
+  }, []);
+
+  if (!isFontLoaded) {
+    console.log("FONTERROR")
+    return null; // You can render a loading indicator here
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.subHeading}>Homepage</Text>
+      <Text style={styles.subHeading}>Welcome, Psycho</Text>
+      <Text style={styles.poetryOfDay}>Poetry of the Day</Text>
+<View style={styles.box}>
+<Text style={styles.poetryOfDay}>
+تم تو خود سے بھی خوب صورت ہو
+</Text>
+      <Text style={styles.poetryOfDay}>کس لیے دیکھتی ہو آئینہ</Text>
+      <Text style={styles.poetryOfDay}>Jaun Elia</Text>
+</View>
+
+      <Text style={styles.poetryOfDay}>Categoriess</Text>
+     
      
       <StatusBar style="auto" />
     </View>
@@ -18,13 +54,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
+  box: {
+    backgroundColor: "#F8F4E3",
+    textAlign: "center",
+    padding: 20,
+    borderRadius: 20,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    boxShadow: "10px 10px 14px 3px rgba(0,0,0,51)"
+  },
  
  
 
   subHeading: {
     color: "#162447",
-    fontSize: 18
+    fontSize:25,
+    fontWeight: "bold"
+  },
+  poetryOfDay: {
+    marginVertical:20,
+    fontSize:18,
+    fontWeight: "bold",
   }
 
 });
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
+import Ionicons from '@expo/vector-icons/Ionicons';
 export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text style={styles.subHeading}>Profile View</Text>
+      <Text style={styles.heading}>My Profile</Text>
+     <View style={styles.flexDiv}>
+     <Image style={styles.avatar} source={require("../assets/profile.jpg")} alt='Profile Image'/>
+     <View style={styles.dataDiv}>
+      <Text style={styles.subHeading}>Hussnain Ahmad</Text>
+      <Text>@Psycho</Text>
+     </View>
+     </View>
+
+    <View style={styles.flexDiv}>
+
+
+     <View style={styles.card}>
+    <Text style={styles.cardHeading}>Followers</Text>
+    <Text style={styles.cardNumber}>100</Text>
+     </View>
+
+
+     <View style={styles.card}>
+    <Text style={styles.cardHeading}>Following</Text>
+    <Text style={styles.cardNumber}>0</Text>
+     </View>
+    
      
+     <View style={styles.card}>
+    <Text style={styles.cardHeading}>Streak</Text>
+    <Text style={styles.cardNumber}>9</Text>
+     </View>
+    
+    
+    </View>
+
+    <Text style={styles.subHeading}>Profile</Text>
+
+
+   <View style={styles.settingsContainer}>
+   <View style={styles.flexDiv}>
+      <Text style={styles.icon}>        <Image source={require("../assets/icons/heart.png")}/>
+      </Text>
+      <Text style={styles.normalText}>
+        
+     Add to Favourites
+      </Text>
+    </View>
+
+    
+    <View style={styles.flexDiv}>
+      <Text style={styles.icon}>        <Image source={require("../assets/icons/certificate.png")}/>
+      </Text>
+      <Text style={styles.normalText}>
+        
+    Badges
+      </Text>
+    </View>
+
+
+
+   </View>
+    <Text style={styles.subHeading}>Account</Text>
+
+  <View style={styles.settingsContainer}>
+
+    <View style={styles.flexDiv}>
+      <Text style={styles.icon}>        <Image source={require("../assets/icons/display.png")}/>
+      </Text>
+      <Text style={styles.normalText}>
+        
+    Display Settings
+      </Text>
+    </View>
+
+      
+    <View style={styles.flexDiv}>
+      <Text style={styles.icon}>        <Image source={require("../assets/icons/logout.png")}/>
+      </Text>
+      <Text style={styles.normalText}>
+        
+    Logout
+      </Text>
+    </View>
+
+  </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,15 +98,69 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  settingsContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start"
+  },
+  flexDiv: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    marginHorizontal: 15
   },
 
- 
- 
-
+  dataDiv: {
+    marginHorizontal: 10
+  },
   subHeading: {
     color: "#162447",
-    fontSize: 18
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 10
+  },
+  heading: {
+    color: "#162447",
+    fontSize: 25,
+    marginVertical: 5,
+    fontWeight: "bold"
+
+  },
+  avatar: {
+    width: 70,
+    height: 70,
+    borderRadius: 50
+  },
+  card: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+    backgroundColor: "red",
+    display:"flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0A0F0D",
+    marginHorizontal: 5,
+    marginVertical: 20
+  },
+  cardHeading: {
+    color: "#F8F4E3",
+    fontSize: 15,
+    fontWeight: "bold"
+  },
+  cardNumber: {
+    color: "#F8F4E3",
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  normalText: {
+    fontSize: 15,
+    marginVertical: 5
+  },
+  icon: {
+    marginHorizontal: 2
   }
 
 });
